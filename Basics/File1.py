7165b4bde4c22406a91a26f59dc3003e1dfa5a43
aliazeembukhari <aliazeembukhari93@gmail.com>
2026-04-17 18:47:25 +0500

    Type Casting

--- Basics/File1.py
+++ Basics/File1.py
@@ -3,3 +3,13 @@ a = 5
 b = 9 
 z = a+b 
 print(z)
+
+print(5//2) #floor divide (it means that it will only give the answer in int)
+print(2**3) #it prints the power of 2
+print(5%2) #print the remainder
+
+# inequality checks 
+
+print(1>2)
+print(1!=2)
+print(1==1)
--- Basics/File2.py
+++ Basics/File2.py
@@ -1,7 +1,2 @@
-#aik line ko comment krny ke liya # ka symbol use ho ga 
+# we use number sign symbol # for adding a comment 
 print("hello")
-"""or agr hum na ziyada line ko comment krna ha to phir 
-humain tripple double comas ya single tripple comas ka use krna pry ga"""
-#phir jo bhi in lines ma likha ho ga wo makhzoof(hide) ho ga
-''''lines indent honi chchiyee yani ke sab aik hi jagha sy start hon mtlab ke line ke start ma exta space na ho warna wo 
-python ma error create kry ga or program run ni kry ga '''
